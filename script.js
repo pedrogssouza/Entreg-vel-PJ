2966d0e071039027a6190e224c2d08b48fc960d5
Pedro Gomes <pgsantanadesouza@usp.br>
2021-08-28 17:04:16 -0300

    HTTP request feita e dados passados do JS para o HTML

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//carrossel
+//display de carrossel para os itens
 const gap = 16;
 
 const carousel = document.getElementById("carousel"),
@@ -23,14 +23,12 @@ botao.addEventListener("click", (e) => {
   e.preventDefault();
 });
 
-const items = document.querySelectorAll(".item");
-console.log(items);
-
-items.forEach((item) => {
+function handleDropdownClass(item) {
   //mostrar dados a mais do item
 
   const dropdownButton = item.children[0].children[2].children[1];
   const dropdownData = item.children[0].children[3];
+  const dropdownImg = item.children[0].children[2].children[1];
 
   let hide = true;
 
@@ -38,14 +36,56 @@ items.forEach((item) => {
     if (hide) {
       dropdownData.classList.add("item-dropdown-data-show");
       dropdownData.classList.remove("item-dropdown-data-hide");
+      dropdownImg.setAttribute("src", "assets/up-arrow-svgrepo-com (1).svg");
     } else {
       dropdownData.classList.remove("item-dropdown-data-show");
       dropdownData.classList.add("item-dropdown-data-hide");
+      dropdownImg.setAttribute("src", "assets/down-arrow-svgrepo-com.svg");
     }
   }
 
   dropdownButton.addEventListener("click", () => {
-    hide = !hide;
     toggleDropdownDataClass();
+    hide = !hide;
   });
+}
+
+function transferDataFromJsToHtml(item, data) {
+  //passar dados do js para o html
+  const item_content = item.children[0];
+  const [img, title, price, dropdownData] = item_content.children;
+
+  img.setAttribute("src", `${data.foto}`);
+  title.innerHTML = data.nome;
+
+  const formattedPrice = data.preco.replace(".", ",").padEnd(5, "0");
+  price.children[0].innerHTML = `R$ ${formattedPrice}`;
+
+  dropdownData.children[0].innerHTML = `Intensidade : ${data.intensidade}`;
+  dropdownData.children[1].innerHTML = data.descricao;
+}
+
+function handleEachItem(item, data) {
+  transferDataFromJsToHtml(item, data);
+  handleDropdownClass(item);
+}
+
+//http request
+
+async function getData(id) {
+  const response = await fetch(
+    `https://entregavel.polijrinternal.com/produtos/${id}`
+  );
+
+  const data = await response.json();
+  return data;
+}
+
+const items = document.querySelectorAll(".item");
+let id = -1;
+
+items.forEach(async (item) => {
+  id += 1;
+  const data = await getData(id);
+  handleEachItem(item, data);
 });
